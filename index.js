3e672d44659b6d5140d3d9dc1547b3b5bd09afa7
Helen Du <helendu.614@gmail.com>
2017-10-18 22:17:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function subtract(a, b) {
 function multiply(a, b) {
    return a * b
  }
- 
+
  function divide(a, b) {
    if(b == 0){
      return NaN
@@ -21,20 +21,19 @@ function multiply(a, b) {
      return a / b
    }
  }
- 
+
  function inc(n) {
    return n
  }
- 
+
  function dec(n) {
    return -- n
  }
- 
+
  function makeInt(n) {
    return parseInt(n, 10)
  }
- 
+
  function preserveDecimal(n) {
    return parseFloat(n, 10)
  }
- 
